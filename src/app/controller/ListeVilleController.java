1d8207178d96edeb4a056d5f53c910815565d736
hai-khawla <khawlahai1972@gmail.com>
2021-04-13 10:37:35 +0200

    test1

--- src/app/controller/ListeVilleController.java
+++ src/app/controller/ListeVilleController.java
@@ -1,4 +1,20 @@
 package app.controller;
 
-public class ListeVilleController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListeVilleController implements Initializable {
+    @FXML
+    ComboBox<String> combo;
+    ObservableList<String> list = FXCollections.observableArrayList("Strasbourg","Mulhouse","Charleville");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        combo.setItems(list);
+    }
 }
